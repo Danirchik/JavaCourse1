78f190a2d6595b00cf359662d19f7d6126855b11
DanirKarimov <danirsnebes@gmail.com>
2025-04-25 17:53:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @author Danir
+ * @version 1.0.2025
+ *
+ */
+
+/// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main { //публичный класс Main
+    public static void main(String[] args) { //Точка входа в программу
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        //sout
+
+        /*
+        hdhshs
+        dghjdg
+        ggdhjh
+         */
+        System.out.println("my name is Danir!");
+
+
+
+    }
+}
