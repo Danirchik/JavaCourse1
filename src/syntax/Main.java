0e2aa5c0f916decddea8e4dcb3a1aca27ccc00c7
DanirKarimov <danirsnebes@gmail.com>
2025-04-25 18:30:49 +0300

    new package

--- src/syntax/Main.java
+++ src/syntax/Main.java
@@ -1,4 +1,4 @@
-/**
+package syntax; /**
  * @author Danir
  * @version 1.0.2025
  *
@@ -6,7 +6,7 @@
 
 /// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main { //публичный класс Main
+public class Main { //публичный класс syntax.Main
     public static void main(String[] args) { //Точка входа в программу
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
